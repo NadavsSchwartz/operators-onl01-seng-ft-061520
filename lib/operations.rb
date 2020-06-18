9b64c61582499f93a4ba7aa799075e6694242093
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-18 14:47:10 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,10 @@
 def unsafe?(speed)
-
+  return true if speed > 60 || speed < 40
+  return false if speed < 60 || speed > 40
 end
 
-
-
 def not_safe?(speed)
-	
+  return speed < 40	? true : false || speed > 60 ? true : false
 end
 	
 
